cbfb7c4e20221a9d3ce1f6a7ebe7e74b20446080
Andres Manco <andresmanco@gmail.com>
2021-01-05 00:57:27 -0600

    Class #10

---
+++ Controllers/EscuelaController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using platzi_asp_net_core.Models;
+using System;
+
+namespace platzi_asp_net_core.Controllers
+{
+    public class EscuelaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var escuela = new Escuela();
+            escuela.AñoFundacion = 2005;
+            escuela.EscuelaId = Guid.NewGuid().ToString();
+            escuela.Nombre = "Platzi School";
+            return View(escuela);
+        }
+    }
+}
